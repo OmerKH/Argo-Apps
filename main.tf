5495edef2cc85ffe0c7c1a3916c54635a659bf9b
Omer Kirshner <omer.khs@gmail.com>
2025-08-13 15:20:13 +0300

    iac_2048

---
+++ main.tf
@@ -0,0 +1,21 @@
+# module "k8s" {
+#   source     = "./modules/k8s"
+#   kubeconfig = var.kubeconfig
+# }
+
+module "argocd" {
+  source        = "./modules/argocd"
+  namespace     = "argocd"
+  chart_version = "4.10.8"
+  node_port     = 30080
+}
+
+module "app_2048" {
+  source            = "./modules/apps"
+  app_name          = "helming2048"
+  argocd_namespace  = "argocd"
+  app_namespace     = "myapp"
+  repo_url          = "https://github.com/OmerKH/Helming2048"
+  revision          = "HEAD"
+  chart_path        = "gamechart"
+}
