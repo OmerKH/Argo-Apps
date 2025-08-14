2216778f783958c4ac5f4e9d899a1751c8e98a1e
Omer Kirshner <omer.khs@gmail.com>
2025-08-14 18:11:21 +0300

    added the correct file

--- main.tf
+++
@@ -1,9 +0,0 @@
-module "app_2048" {
-  source            = "./modules/2048"
-  app_name          = "helming2048"
-  argocd_namespace  = "argocd"
-  app_namespace     = "myapp"
-  repo_url          = "https://github.com/OmerKH/Helming2048"
-  revision          = "HEAD"
-  chart_path        = "gamechart"
-}
